830ad650de0bae93132e6e69ea13a2ba15bc063f
Zhongkeyang <1670774483@qq.com>
2024-05-14 19:02:30 +0800

    create agent_utils.py and modify the gemini_vision_follower_atomic.py

---
+++ src/cb2game/agents/agent_utils.py
@@ -0,0 +1,188 @@
+import functools
+
+from cb2game.pyclient.game_endpoint import Action, GameState
+import logging
+import time
+import json
+from cb2game.server.db_tools import follower_view_description
+logger = logging.getLogger(__name__)
+def actions_from_code(action_code, i_uuid: str = None):
+    # Split action code by comma.
+    characters_in_prompt = action_code.split(",")
+    if len(characters_in_prompt) == 0:
+        # logger.warning("Empty action string.")
+        return None
+    actions = []
+    for c in characters_in_prompt:
+        # Convert to lower and strip whitespace.
+        c = c.lower().strip()
+        if "forward".startswith(c) or "f".startswith(c):
+            actions.append(Action.Forwards())
+        elif "forward" in c:
+            actions.append(Action.Forwards())
+        elif "backward".startswith(c) or "b".startswith(c):
+            actions.append(Action.Backwards())
+        elif "backward" in c:
+            actions.append(Action.Backwards())
+        elif "left".startswith(c) or "l".startswith(c):
+            actions.append(Action.Left())
+        elif "left" in c:
+            actions.append(Action.Left())
+        elif "right".startswith(c) or "r".startswith(c):
+            actions.append(Action.Right())
+        elif "right" in c:
+            actions.append(Action.Right())
+        elif "around" in c:
+            actions.append(Action.Right())
+            actions.append(Action.Right())
+            actions.append(Action.Right())
+        elif "done".startswith(c) or "d".startswith(c):
+            actions.append(Action.InstructionDone(i_uuid))
+        elif "noop" in c:
+            actions.append(Action.NoopAction())
+        else:
+            logger.warning(f"Invalid action code: {c}")
+    return actions
+
+def find_matching_tiles(data, keyword):# keyword-->Next Location: Tile at heading  <angle> and distance <distance>: <TILE_TYPE>
+    if "distance 0.0" in keyword:
+        return ""
+    lines = data.split('\n')
+    matching_line = ""
+    found = False
+    for i, line in enumerate(lines):
+        if keyword.lower() in line.lower():
+            if "You are standing here." in lines[i+2]:
+                return ""
+            for j in range(i+2, i+9):
+                if j < len(lines) and "cannot reach" not in lines[j]:
+                    matching_line = lines[j].split(":")[1]
+                    break
+            break
+    return matching_line
+
+def deselect_card(mapu, description_atomic, follower, card_location):
+    # 1.卡片在当前位置：forward+backward+forward或者backward+forward+backward
+    if "distance 0:" in card_location:
+        # 还要考虑follower的朝向
+        follower_orientation = follower.heading_degrees() - 60
+        atomic_instructions = []
+        # 检查周围的tile是否可行
+        # 可行的tile.name: "GROUND_TILE":3, "GROUND_TILE_PATH":28,"MOUNTAIN_TILE":30,"RAMP_TO_MOUNTAIN":31,"SNOWY_MOUNTAIN_TILE":32,"SNOWY_RAMP_TO_MOUNTAIN":36
+        actionable_tiles_id = [3, 28, 30, 31, 32, 36]
+        for neighbors_location in follower.location().neighbors():
+            # 检索该tile的名字，并判断是否在可行的tile中
+            neighbors_tile_id = mapu.get_tile_id(neighbors_location)
+            if neighbors_tile_id not in actionable_tiles_id:
+                continue
+            # 如果当前follower不在mountain类的tile上，那么只能选择GROUND_TILE和GROUND_TILE_PATH、RAMP_TO_MOUNTAIN、SNOWY_RAMP_TO_MOUNTAIN
+            if mapu.get_tile_id(follower.location()) in [3, 28]:
+                if neighbors_tile_id not in [3, 28, 31, 36]:
+                    continue
+            # 如果当前follower在mountain类的tile上，那么只能选择MOUNTAIN_TILE、SNOWY_MOUNTAIN_TILE、RAMP_TO_MOUNTAIN、SNOWY_RAMP_TO_MOUNTAIN
+            if mapu.get_tile_id(follower.location()) in [30, 32]:
+                if neighbors_tile_id not in [30, 32, 31, 36]:
+                    continue
+            # 如果当前follower在RAMP上，志强选择""forward, backward, forward"或者"backward, forward, backward"
+            if mapu.get_tile_id(follower.location()) in [31, 36]:
+                # 先不考虑ramp的朝向
+                atomic_instructions.append("forward, backward, forward")
+                continue
+            degrees_away = follower.location().degrees_to(neighbors_location) - follower_orientation
+            if degrees_away < 0:
+                degrees_away += 360
+            if degrees_away > 180:
+                degrees_away -= 360
+            if degrees_away == 0:
+                atomic_instructions.append("forward, backward")
+            elif degrees_away == 60:
+                atomic_instructions.append("right, forward, backward")
+            elif degrees_away == 120:
+                atomic_instructions.append("left, backward, forward")
+                #atomic_instructions.append("right, right, forward, backward")
+            elif degrees_away == 180:
+                atomic_instructions.append("backward, forward, backward")
+            elif degrees_away == -60:
+                atomic_instructions.append("left, forward, backward")
+            elif degrees_away == -120:
+                atomic_instructions.append("right, backward, forward")
+                #atomic_instructions.append("left, left, forward, backward")
+        return atomic_instructions[0] # 返回第一个可行的tile，的方式
+    # 2.卡片不在当前位置：直接去卡片的位置
+    else:
+        atomic_instruction = find_matching_tiles(description_atomic, card_location)
+        return atomic_instruction
+
+def get_action_string(response_dict, mapu, prop_update, follower):
+    """
+    Args:
+        response_dict: response from LLM
+        mapu: the map_update of the whole game
+        prop_update: the prop_update in follower's view
+        follower:
+
+    Returns:
+        action_string: the atomic instruction string to be executed
+
+    """
+    deferred_task = response_dict["Deferred Task"]
+    immediate_task = response_dict["Immediate Task"]
+    # try: # 可能输出不规范
+    #     response_dict = json.loads(response_text)
+    #     response_dict = json.loads(response_text)
+    #     self.deferred_task = response_dict["Deferred Task"]
+    #     immediate_task = response_dict["Immediate Task"]
+    # except KeyError as e:
+
+    # 只要去具体位置才广搜
+    description_atomic = ""
+    action_string = ""
+    if "Card Interaction" in immediate_task or "Next Location" in immediate_task:  # Type1: Change Direction
+        start_time = time.time()
+        print("search tiles length: ", len(mapu.tiles))
+        description_atomic = follower_view_description.DescribeMap(mapu, prop_update.props, follower.location(),
+                                                                   follower.heading_degrees())
+        end_time = time.time()
+        print("Time taken to search map: ", end_time - start_time)
+        with open("description_atomic.txt", "w") as f:
+            f.write(description_atomic)
+
+    if "Change Direction" in immediate_task:  # Type1: Change Direction
+        action_string = immediate_task.split(":")[1].strip()
+    elif "Card Interaction" in immediate_task:  # Type3: Next Location
+        card_location = immediate_task.split("Card at")[1].strip()
+        if "Deselect" in immediate_task:
+            action_string = deselect_card(mapu, description_atomic, follower, card_location)
+        elif "Select" in immediate_task:
+            action_string = find_matching_tiles(description_atomic, card_location)
+
+    elif "Next Location" in immediate_task:  # Type3: Next Location
+        action_string = find_matching_tiles(description_atomic, immediate_task.split("Next Location:")[1].strip())
+    else:
+        action_string = "done"
+    if deferred_task == "NULL":
+        action_string += ",done"
+    return action_string
+
+
+def delay_execution(delay_time):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            time.sleep(delay_time)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+def timeout_decorator(timeout):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future = executor.submit(func, *args, **kwargs)
+                try:
+                    return future.result(timeout)
+                except concurrent.futures.TimeoutError:
+                    print("Function call timed out")
+        return wrapper
+
+    return decorator
--- src/cb2game/agents/gemini_vision_follower_atomic.py
+++ src/cb2game/agents/gemini_vision_follower_atomic.py
@@ -35,7 +35,7 @@ from cb2game.pyclient.follower_data_masking import (
     CensorFollowerMap,
     CensorFollowerProps,
 )
-
+from cb2game.agents.agent_utils import *
 
 logger = logging.getLogger(__name__)
 class Config():
@@ -144,10 +144,6 @@ class GeminiVisionFollowerAtomic(Agent):
         # 保存第一视角图片
         # game.visualization().visualize_follower_visibility(self.turn_number, action_number)
         game.visualization().visualize_follower_visibility(0, 0)
-        # image_data = PIL.Image.open(
-        #     crop_non_white_square(f"follower_view/follower_visibility_{self.turn_number}_{action_number}.png"))
-        # image_data = Path(crop_non_white_square(
-        #     f"follower_view/follower_visibility_{self.turn_number}_{action_number}.png")).read_bytes()
         first_view_image_path = crop_non_white_square(f"follower_view/follower_visibility_0_0.png")
         image_data = Path(first_view_image_path).read_bytes()
         self.image_parts.append({"mime_type": "image/png", "data": image_data})
@@ -197,42 +193,11 @@ class GeminiVisionFollowerAtomic(Agent):
         print("response:\n",response_text)
         response_dict = json.loads(response_text)
         self.deferred_task = response_dict["Deferred Task"]
-        immediate_task = response_dict["Immediate Task"]
-        # try: # 可能输出不规范
-        #     response_dict = json.loads(response_text)
-        #     response_dict = json.loads(response_text)
-        #     self.deferred_task = response_dict["Deferred Task"]
-        #     immediate_task = response_dict["Immediate Task"]
-        # except KeyError as e:
 
-
-        # 只要去具体位置才广搜
-        if "Card Interaction" in immediate_task or "Next Location" in immediate_task: # Type1: Change Direction
-            start_time = time.time()
-            print("search tiles length: ",len(mapu.tiles))
-            description_atomic = follower_view_description.DescribeMap(mapu, prop_update.props, instrs, follower.location(), follower.heading_degrees())
-            end_time = time.time()
-            print("Time taken to search map: ", end_time-start_time)
-            with open("description_atomic.txt", "w") as f:
-                f.write(description_atomic)
-
-        if "Change Direction" in immediate_task: # Type1: Change Direction
-            action_string = immediate_task.split(":")[1].strip()
-        elif "Card Interaction" in immediate_task: #Type3: Next Location
-            card_location = immediate_task.split("Card at")[1].strip()
-            if "Deselect" in immediate_task:
-                action_string = deselect_card(mapu, description_atomic, follower, card_location)
-            elif "Select" in immediate_task:
-                action_string = find_matching_tiles(description_atomic, card_location)
-
-        elif "Next Location" in immediate_task: #Type3: Next Location
-            action_string = find_matching_tiles(description_atomic, immediate_task.split("Next Location:")[1].strip())
-        else:
-            action_string = "done" 
-        if self.deferred_task == "NULL":
-            action_string += ",done"
+        action_string = get_action_string(response_dict, mapu, prop_update, follower)
         active_instruction = get_active_instruction(instrs)
         actions = actions_from_code(action_string, active_instruction.uuid)
+
         if len(actions) == 0:
             return Action.NoopAction()
         if self.queueing_enabled:
@@ -241,148 +206,13 @@ class GeminiVisionFollowerAtomic(Agent):
 
         return actions[0]
 
-# 存储历史信息
-def append_messages_to_file(filename, messages, turn_number):
-    with open(filename, "a") as file:
-        file.write(str(turn_number)+":\n")
-        for message in messages:
-            file.write(str(message))
-        file.write("\n \n")
 
-def timeout_decorator(timeout):
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                future = executor.submit(func, *args, **kwargs)
-                try:
-                    return future.result(timeout)
-                except concurrent.futures.TimeoutError:
-                    print("Function call timed out")
-        return wrapper
-
-    return decorator
-
-def delay_execution(delay_time):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            time.sleep(delay_time)
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
 # @timeout_decorator(timeout=120)
 # @delay_execution(delay_time=10)
 def call_gemini_api_sync(messages, model):
     response = model.generate_content(messages)
     return response
 
-def deselect_card(mapu, description_atomic, follower, card_location):
-    # 1.卡片在当前位置：forward+backward+forward或者backward+forward+backward
-    if "distance 0:" in card_location:
-        # 还要考虑follower的朝向
-        follower_orientation = follower.heading_degrees() - 60
-        atomic_instructions = []
-        # 检查周围的tile是否可行
-        # 可行的tile.name: "GROUND_TILE":3, "GROUND_TILE_PATH":28,"MOUNTAIN_TILE":30,"RAMP_TO_MOUNTAIN":31,"SNOWY_MOUNTAIN_TILE":32,"SNOWY_RAMP_TO_MOUNTAIN":36
-        actionable_tiles_id = [3, 28, 30, 31, 32, 36]
-        for neighbors_location in follower.location().neighbors():
-            # 检索该tile的名字，并判断是否在可行的tile中
-            neighbors_tile_id = mapu.get_tile_id(neighbors_location)
-            if neighbors_tile_id not in actionable_tiles_id:
-                continue
-            # 如果当前follower不在mountain类的tile上，那么只能选择GROUND_TILE和GROUND_TILE_PATH、RAMP_TO_MOUNTAIN、SNOWY_RAMP_TO_MOUNTAIN 
-            if mapu.get_tile_id(follower.location()) in [3, 28]:
-                if neighbors_tile_id not in [3, 28, 31, 36]:
-                    continue
-            # 如果当前follower在mountain类的tile上，那么只能选择MOUNTAIN_TILE、SNOWY_MOUNTAIN_TILE、RAMP_TO_MOUNTAIN、SNOWY_RAMP_TO_MOUNTAIN
-            if mapu.get_tile_id(follower.location()) in [30, 32]:
-                if neighbors_tile_id not in [30, 32, 31, 36]:
-                    continue
-            # 如果当前follower在RAMP上，志强选择""forward, backward, forward"或者"backward, forward, backward"
-            if mapu.get_tile_id(follower.location()) in [31, 36]:
-                # 先不考虑ramp的朝向
-                atomic_instructions.append("forward, backward, forward")
-                continue 
-            degrees_away = follower.location().degrees_to(neighbors_location) - follower_orientation
-            if degrees_away < 0:
-                degrees_away += 360
-            if degrees_away > 180:
-                degrees_away -= 360
-            if degrees_away == 0:
-                atomic_instructions.append("forward, backward")
-            elif degrees_away == 60:
-                atomic_instructions.append("right, forward, backward")
-            elif degrees_away == 120:
-                atomic_instructions.append("left, backward, forward")
-                #atomic_instructions.append("right, right, forward, backward")
-            elif degrees_away == 180:
-                atomic_instructions.append("backward, forward, backward")
-            elif degrees_away == -60:
-                atomic_instructions.append("left, forward, backward")
-            elif degrees_away == -120:
-                atomic_instructions.append("right, backward, forward")
-                #atomic_instructions.append("left, left, forward, backward")
-        return atomic_instructions[0] # 返回第一个可行的tile，的方式
-    # 2.卡片不在当前位置：直接去卡片的位置
-    else:
-        atomic_instruction = find_matching_tiles(description_atomic, card_location)
-        return atomic_instruction
-    
-
-def find_matching_tiles(data, keyword):# keyword-->Next Location: Tile at heading  <angle> and distance <distance>: <TILE_TYPE>
-    if "distance 0.0" in keyword:
-        return ""
-    lines = data.split('\n')
-    matching_line = ""
-    found = False
-    for i, line in enumerate(lines):
-        if keyword.lower() in line.lower():
-            if "You are standing here." in lines[i+2]:
-                return ""
-            for j in range(i+2, i+9):
-                if j < len(lines) and "cannot reach" not in lines[j]:
-                    matching_line = lines[j].split(":")[1]
-                    break
-            break
-    return matching_line
-
-def actions_from_code(action_code, i_uuid: str = None):
-    # Split action code by comma.
-    characters_in_prompt = action_code.split(",")
-    if len(characters_in_prompt) == 0:
-        # logger.warning("Empty action string.")
-        return None
-    actions = []
-    for c in characters_in_prompt:
-        # Convert to lower and strip whitespace.
-        c = c.lower().strip()
-        if "forward".startswith(c) or "f".startswith(c):
-            actions.append(Action.Forwards())
-        elif "forward" in c:
-            actions.append(Action.Forwards())
-        elif "backward".startswith(c) or "b".startswith(c):
-            actions.append(Action.Backwards())
-        elif "backward" in c:
-            actions.append(Action.Backwards())
-        elif "left".startswith(c) or "l".startswith(c):
-            actions.append(Action.Left())
-        elif "left" in c:
-            actions.append(Action.Left())
-        elif "right".startswith(c) or "r".startswith(c):
-            actions.append(Action.Right())
-        elif "right" in c:
-            actions.append(Action.Right())
-        elif "around" in c:
-            actions.append(Action.Right())
-            actions.append(Action.Right())
-            actions.append(Action.Right())
-        elif "done".startswith(c) or "d".startswith(c):
-            actions.append(Action.InstructionDone(i_uuid))
-        elif "noop" in c:
-            actions.append(Action.NoopAction())
-        else:
-            logger.warning(f"Invalid action code: {c}")
-    return actions
 
 
 def get_active_instruction(instructions):
